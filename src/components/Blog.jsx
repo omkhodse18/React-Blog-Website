92f1b4c0700aa36a0c204c89f0eab094898a930c
omkhodse18 <omkarkhodse.18@gmail.com>
2024-03-21 01:51:08 +0530

    Update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -10,7 +10,7 @@ function Blog() {
   return (
     // <div className='w-11/12 max-w-[670px] h-screen py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px]  justify-center items-center'>
 
-    <div className='w-11/12 max-w-[670px] h-full py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center'>    
+    <div className='w-11/12 max-w-[670px] h-[full] py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center'>    
         {
             loading ? (<Spinner/>) : (
                 (posts.length === 0) ? (<div><p>No post found</p></div>) 
